e05a2c6939b5d6406006e83dcf16d319680e0b52
Robert Mikes <robertm@titusgt.com>
2021-04-06 12:01:59 +0000

    Load data into PostgreSQL

---
+++ data/create_and_loadcsv.postgres.sh
@@ -0,0 +1,3 @@
+psql -U postgres -h 192.168.88.18 rob_test_btable -f schema.postgres.sql
+psql -U postgres -h 192.168.88.18 rob_test_btable -f loadcsv.postgres.sql
+
---
+++ data/loadcsv.postgres.sql
@@ -0,0 +1,14 @@
+\COPY compras FROM 'compras.csv' CSV HEADER;
+
+
+\COPY produtos FROM 'produtos.csv' CSV HEADER;
+
+
+\COPY tempo FROM 'tempo.csv' CSV HEADER;
+
+
+\COPY vendas FROM 'vendas.csv' CSV HEADER;
+
+
+\COPY vendedores FROM 'vendedores.csv' CSV HEADER;
+
